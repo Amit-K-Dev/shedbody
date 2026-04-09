7e020e8628ff612b02d49ff3bbf2db13ed3623aa
Amit Kumar <kumar.amit2648@gmail.com>
2026-04-10 03:52:09 +0530

    fix: align items to center (logo) for UI/UX

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,19 +1,21 @@
 import { BicepsFlexed } from "lucide-react";
-import Link from "next/link";
 
 export default function AuthLayout({ children }) {
   return (
     <section className="py-16">
       <h1 className="text-2xl font-bold text-zinc-50 text-center mb-6">
         {/* Logo */}
-        <Link href="/" className="flex items-center tracking-wide group">
+        <a
+          href="/"
+          className="flex items-center justify-center tracking-wide group"
+        >
           <BicepsFlexed
             size={22}
             className="text-emerald-400 mr-2 group-hover:scale-110 transition"
           />
           <span className="text-zinc-50">Shed</span>
           <span className="text-emerald-400">Body</span>
-        </Link>
+        </a>
       </h1>
 
       <div className="min-h-screen flex items-center justify-center bg-zinc-950 text-zinc-50 px-4">
